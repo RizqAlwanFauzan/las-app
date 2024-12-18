f9ec38c72e9c6de625835442ded61ca3823935c1
Rizq Alwan Fauzan <r.alwanfauzan@gmail.com>
2024-12-18 23:40:26 +0700

    Install Laravel v11.36.1 (PHP v8.3.7)

---
+++ lang/id/pagination.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris Bahasa Pagination
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut digunakan oleh pustaka paginator untuk membangun
+    | tautan pagination sederhana. Anda bebas untuk mengubahnya sesuai dengan
+    | keinginan Anda untuk menyesuaikan tampilan Anda agar lebih sesuai dengan
+    | aplikasi Anda.
+    |
+    */
+
+    'previous' => '&laquo; Sebelumnya',
+    'next' => 'Selanjutnya &raquo;',
+
+];
